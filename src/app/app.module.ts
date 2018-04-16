993a9f11932f2258373d0792712e030a4b02d26c
TharunSadula <sadulatn@mail.uc.edu>
2018-04-16 00:27:45 -0400

    removed angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -25,8 +24,6 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatCheckboxModule,
     RouterModule.forRoot([{
       path:'',
       component:DashboardComponent
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  private  tarun:any[];
   constructor() { }
 
   ngOnInit() {
+   this.tarun=[{id:1,jobDescription:"Neudesic",date:Date.now,appliedIn:"Linkedin"},
+   {id:2,jobDescription:"deloitee",date:Date.now,appliedIn:"glassdoor"},
+  {id:1,jobDescription:"Djj",date:Date.now,appliedIn:"Indeed"}]
   }
 
 }
