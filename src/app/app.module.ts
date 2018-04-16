2413e460e65baa51790b761f2dcd138770cc9432
TharunSadula <sadulatn@mail.uc.edu>
2018-04-15 20:52:55 -0400

    routing Configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    NotFoundComponent,
+    JobsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    RouterModule.forRoot([{
+      path:'',
+      component:DashboardComponent
+    },
+    {
+      path:'dashboard',
+      component:DashboardComponent
+    }, {
+      path:'jobs',
+      component:JobsComponent
+    }, {
+      path:'login',
+      component:LoginComponent
+    },
+    {
+      path:'**',
+      component:NotFoundComponent
+    }]),
   ],
   providers: [],
   bootstrap: [AppComponent]
